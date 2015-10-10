3b9a479a01b894853e9317ab2c329da9c3e47586
75py <dev.75py@gmail.com>
2015-10-10 20:08:13 +0900

    constants

---
+++ app/src/main/kotlin/com/nagopy/android/aplin/constants/VersionCode.kt
@@ -0,0 +1,25 @@
+package com.nagopy.android.aplin.constants
+
+
+/**
+ * {@link android.os.Build.VERSION_CODES}
+ */
+public object VersionCode {
+
+    public val BASE: Int = 1
+    public val GINGERBREAD_MR1: Int = 10
+    public val HONEYCOMB: Int = 11
+    public val HONEYCOMB_MR1: Int = 12
+    public val HONEYCOMB_MR2: Int = 13
+    public val ICE_CREAM_SANDWICH: Int = 14
+    public val ICE_CREAM_SANDWICH_MR1: Int = 15
+    public val JELLY_BEAN: Int = 16
+    public val JELLY_BEAN_MR1: Int = 17
+    public val JELLY_BEAN_MR2: Int = 18
+    public val KITKAT: Int = 19
+    public val KITKAT_WATCH: Int = 20
+    public val LOLLIPOP: Int = 21
+    public val LOLLIPOP_MR1: Int = 22
+    public val MARSHMALLOW: Int = 23
+
+}
