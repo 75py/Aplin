445b4a9d68345753ddc0e7394722a9e350a58db9
75py <dev.75py@gmail.com>
2015-11-22 21:05:00 +0900

    Modify script
    [ci skip]

--- deploy_beta.sh
+++ deploy_beta.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo $DEPLOY_BETA
+echo ${DEPLOY_BETA}
 
-if [ $DEPLOY_BETA = 1 ]; then
+if [ ${DEPLOY_BETA} = 1 ]; then
   echo "Deploy Beta"
   ./gradlew crashlyticsUploadDistributionDebug -PdisablePreDex --stacktrace --info
 else
