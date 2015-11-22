17b7deb6ad0823ff36e1706fc56cf1be55436c5f
75py <dev.75py@gmail.com>
2015-11-22 19:20:10 +0900

    Add deploy script

---
+++ .buildscript/deploy_beta.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo $DEPLOY_BETA
+
+if [ $DEPLOY_BETA = 1 ]; then
+  echo "Deploy Beta"
+  ../gradlew crashlyticsUploadDistributionDebug -PdisablePreDex --stacktrace --info
+else
+  echo "DEPLOY_BETA is not 1"
+fi
+
