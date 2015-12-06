94621a773dbcbb085e4a85546d0ffbf0026b8a15
75py <dev.75py@gmail.com>
2015-12-06 18:23:57 +0900

    Update deploy script

--- deploy_beta.sh
+++ deploy_beta.sh
@@ -5,8 +5,9 @@ echo "TRAVIS_PULL_REQUEST: ${TRAVIS_PULL_REQUEST}"
 
 if [ ${TRAVIS_BRANCH} = "master" -a ${TRAVIS_PULL_REQUEST} = "false" ]; then
   echo "Deploy Beta"
-  MGS=``
-  ./gradlew crashlyticsUploadDistributionProductionDebug -PdisablePreDex --stacktrace --info
+  MSG=`git log -1 --pretty='%s'`
+  echo ${MSG}
+  ./gradlew crashlyticsUploadDistributionProductionDebug -PdisablePreDex --stacktrace --info -DbetaDistributionReleaseNotes="${MSG}"
 else
   echo "Skip deployment"
 fi
