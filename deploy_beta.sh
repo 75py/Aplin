5f886104e3143ff5129e58a317c6c1e313e6032a
75py <dev.75py@gmail.com>
2015-12-06 14:10:23 +0900

    Update deploy script [ci skip]

--- deploy_beta.sh
+++ deploy_beta.sh
@@ -2,10 +2,14 @@
 
 echo "TRAVIS_BRANCH: ${TRAVIS_BRANCH}"
 echo "TRAVIS_PULL_REQUEST: ${TRAVIS_PULL_REQUEST}"
+echo "TRAVIS_TAG: ${TRAVIS_TAG}"
 
-if [ ${TRAVIS_BRANCH} = "master" -a ${TRAVIS_PULL_REQUEST} = "false" ]; then
+
+if [ -n "${TRAVIS_TAG}" ]; then
   echo "Deploy Beta"
-  ./gradlew crashlyticsUploadDistributionProductionDebug -PdisablePreDex --stacktrace --info
+  MSG=`git show ${TRAVIS_TAG}`
+  echo "${MSG}"
+  ./gradlew crashlyticsUploadDistributionProductionDebug -PdisablePreDex --stacktrace --info -DbetaDistributionReleaseNotes="${MSG}"
 else
   echo "Skip deployment"
 fi
