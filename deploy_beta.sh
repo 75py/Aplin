3fe68da65d8ea1f4ab74d4791c3bd3965c6e8e10
75py <dev.75py@gmail.com>
2015-12-18 23:34:44 +0900

    Update ci script

--- deploy_beta.sh
+++ deploy_beta.sh
@@ -2,8 +2,9 @@
 
 echo "TRAVIS_BRANCH: ${TRAVIS_BRANCH}"
 echo "TRAVIS_PULL_REQUEST: ${TRAVIS_PULL_REQUEST}"
+echo "DEPLOY_BETA: ${DEPLOY_BETA}"
 
-if [ ${TRAVIS_BRANCH} = "master" -a ${TRAVIS_PULL_REQUEST} = "false" ]; then
+if [ ${TRAVIS_BRANCH} = "master" -a ${TRAVIS_PULL_REQUEST} = "false" -a ${DEPLOY_BETA} = "true" ]; then
   echo "Deploy Beta"
   MSG=`git log -1 --pretty='%s'`
   echo ${MSG}
